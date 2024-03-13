2f6841a708e05a442cdca8b80cbc27ecfc28e82b
SondosEmara <84973376+SondosEmara@users.noreply.github.com>
2024-03-13 03:28:23 +0200

    Version_1_Trips Display Cards With Filters Search
    Search By Cost , Trip Type , Country , and Date

---
+++ Travelista/ViewModel/TripViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Travelista.Models;
+namespace Travelista.ViewModel
+{
+    public class TripViewModel
+    {
+       public List<Trip> trips { set; get; }
+        public List<TripType> tripTypes { set; get; }
+        public List<Country> Countries { set; get; }
+    }
+}
